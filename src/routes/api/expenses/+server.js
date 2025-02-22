5282091b1bab4f7a43a7a91f4a0f480b934d8a8a
Henri Branken <henri.branken777@gmail.com>
2025-02-22 18:44:50 +0200

    Neatened the +server.js file.

--- src/routes/api/expenses/+server.js
+++ src/routes/api/expenses/+server.js
@@ -1,10 +1,2 @@
-import Expense from '$lib/server/models/Expense';
+import Expense from '$lib/server/models/Expense.js';
 import { connectDB } from '$lib/server/db';
-
-/*
-// GET Find all the Expenses
-export async function GET() {}
-
-// POST:  Add a new Expense.
-export async function POST({ reqeust }) {}
-*/
