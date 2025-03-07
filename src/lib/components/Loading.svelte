8d7971ddca137d3afea1f4a6bc05463a0fb09e1c
Henri Branken <henri.branken777@gmail.com>
2025-03-07 16:37:27 +0200

    Added a Modal Spinner to indicate when the page is loading. . .

--- src/lib/components/Loading.svelte
+++ src/lib/components/Loading.svelte
@@ -1,14 +1,37 @@
-<div>Loading. . .</div>
+<div class="loading-backdrop">
+	<div class="spinner"></div>
+</div>
 
 <style>
-	div {
+	.loading-backdrop {
 		position: fixed;
+		top: 0;
 		left: 0;
-		top: 30%;
-		margin-left: 12px;
-		font-size: 2.3rem;
-		font-weight: bold;
-		text-align: left;
-		color: #777;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.8); /* Semi-transparent background */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		z-index: 1000;
+	}
+
+	.spinner {
+		width: 80px;
+		height: 80px;
+		border: 12px solid rgba(255, 255, 255, 0.5);
+		border-top: 12px solid white;
+		border-radius: 50%;
+		animation: spin 1000ms linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
 	}
 </style>
