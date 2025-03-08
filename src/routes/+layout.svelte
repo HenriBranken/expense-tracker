97d080997fde2b58d29ce1ec263763ed854f32b5
Henri Branken <henri.branken777@gmail.com>
2025-03-08 19:09:49 +0200

    imported from $lib instead of relative path to lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import '../styles/global.scss';
-	import Header from '../lib/components/Header.svelte';
-	import Footer from '../lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <div class="layout">
